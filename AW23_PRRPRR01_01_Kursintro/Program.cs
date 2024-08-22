531e5a33c24684cde6ba83032a6522a1b50240e5
hannesblaman <hannes@blaman.com>
2024-08-22 12:09:42 +0200

    Add project files.

---
+++ AW23_PRRPRR01_01_Kursintro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AW23_PRRPRR01_01_Kursintro {
+	internal class Program {
+		static void Main(string[] args) {
+
+			Console.WriteLine("Hello world");
+
+		}
+	}
+}
